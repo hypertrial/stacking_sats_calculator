6ac0c31eaa584a82eb724439ce35d811f079d6fc
Matt Faltyn <faltyn.matthew@gmail.com>
2025-06-10 16:40:00 -0700

    Added github actions Page deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/stacking_sats_product/',
   server: {
     open: true,
     port: 3000,
